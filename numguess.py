b78df2f5b0e717d084b7c090f89f5efb2ee4071f
jylsat11 <jylsat11@gmail.com>
2023-01-09 16:29:06 +0900

    Feat: modified file
    there was a grammer error. So I edited it.`

--- numguess.py
+++ numguess.py
@@ -2,6 +2,6 @@ from random import randint
 
 correct_answer = randint(1,100)
 
-user_answer = int(input("1부터 100사이의 값을 입력해주세요:")
+user_answer = int(input("1부터 100사이의 값을 입력해주세요:"))
 
 print(f"입력하신 {user_answer}과 정답인 {correct_answer}가 {user_answer==correct_answer}")
