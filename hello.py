057fd74970ac8ff125c17600f6f30822ff8f25f0
jylsat11 <jylsat11@gmail.com>
2023-01-10 09:35:30 +0900

    feat: Add container (training comma test)
    before

--- hello.py
+++ hello.py
@@ -3,3 +3,11 @@ username = "John Doe"
 
 #Print greeting with username
 print("hello!!,",username)
+
+#Don't do
+animals = ['cat','dog']
+
+# Do (Trailing comma)
+menus = [
+    'hamburger','salad',
+]
