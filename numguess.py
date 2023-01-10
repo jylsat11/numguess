d9bcd321077bb9be3ddaff6761c968198dd3433c
jylsat11 <jylsat11@gmail.com>
2023-01-10 15:08:40 +0900

    feat: add comparing user's guess with answer
    if the value of the input from user is the same with the correct_answer,
    then print "congrat.
    else if the value of the input from user is larger than correct answer,
    then print "It is larger than correct answer."
    else print "It is smaller than correct answer."

--- numguess.py
+++ numguess.py
@@ -1,7 +1,17 @@
 from random import randint
+from time import sleep
 
 correct_answer = randint(1,100)
 
-user_answer = int(input("1부터 100사이의 값을 입력해주세요:"))
+user_answer = int(input("please enter one number between 1 and 100"))
 
-print(f"입력하신 {user_answer}과 정답인 {correct_answer}가 {user_answer==correct_answer}")
+print(f" it is {user_answer==correct_answer} that your answer {user_answer} and {correct_answer} is the same number")
+
+if user_answer == correct_answer:
+    print('**************************************')
+    sleep(1)
+    print(f"Congrat!!!!! the answer is {correct_answer}.")
+elif user_answer > correct_answer:
+    print(f"Wrong answer !! the answer is smaller than your answer {user_answer}")
+elif user_answer < correct_answer:
+    print(f"Wrong answer !! the answer is larger than your answer {user_answer}")
