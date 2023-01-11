e4abb22d3a9e0724ef34c91356fa4227f95d2512
jylsat11 <jylsat11@gmail.com>
2023-01-11 16:33:50 +0900

    feat: add function
    i added two function.
    one is user_answer, the other is numguess.

--- numguess.py
+++ numguess.py
@@ -1,17 +1,35 @@
 from random import randint
 from time import sleep
 
+
+def user_input()->int:
+  user_answer = int(input("please enter one integer number (1~100): "))
+  return user_answer
+
+def numguess(user_answer:int, correct_answer:int):
+  if user_answer == correct_answer:
+      return 'same'
+
+  elif user_answer > correct_answer:
+      return 'larger'
+  else:
+      return 'smaller'
+
+
 correct_answer = randint(1,100)
 
-user_answer = int(input("please enter one number between 1 and 100"))
 
-print(f" it is {user_answer==correct_answer} that your answer {user_answer} and {correct_answer} is the same number")
+for i in range(10+1):
+  user_answer = user_input()
+  result = numguess(user_answer,correct_answer)
+  if result == 'same':
+    print(f"Congrat. your answer {user_answer} and correct answer {correct_answer} is the same number.")
+    break
+  elif result == 'larger':
+    print(f"wrong. your asnwer {user_answer} is larger than the correct answer.")
+    print("please enter the number again (1~100).")
+  else:
+    print(f"wrong. Your answer {user_answer} is smaller than the correct answer.")
+    print("please enter the number again (1~100).")    
 
-if user_answer == correct_answer:
-    print('**************************************')
-    sleep(1)
-    print(f"Congrat!!!!! the answer is {correct_answer}.")
-elif user_answer > correct_answer:
-    print(f"Wrong answer !! the answer is smaller than your answer {user_answer}")
-elif user_answer < correct_answer:
-    print(f"Wrong answer !! the answer is larger than your answer {user_answer}")
+print("The game is over.")
